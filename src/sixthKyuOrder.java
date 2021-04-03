2c475837228661bd1be8a87d430fca999661a3f7
ss2261 <ss2261@kent.ac.uk>
2021-04-03 19:00:53 +0100

    sixthKyuOrder - spilt word into string array and eliminated the blank spaces

--- src/sixthKyuOrder.java
+++ src/sixthKyuOrder.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
 public class sixthKyuOrder {
+    //Instructions
+
+    /*Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+    Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+    If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.*/
+
     public static void main(String[] args) {
-        
+
+        System.out.println(order("is2 Thi1s T4est 3a"));
+    }
+
+    public static String order(String words){
+
+        String[] splitWordsArr = words.split(" ");
+
+        System.out.println("Initial arr: "+Arrays.toString(splitWordsArr));
+
+        for (int x=0; x<words.length(); x++){
+
+
+        }
+
+
+        return null;
+
     }
 }
