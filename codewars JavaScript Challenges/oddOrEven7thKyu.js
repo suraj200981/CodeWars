9833fbc15323b5940483fbb88d0c21a491df57cc
ss2261 <ss2261@kent.ac.uk>
2021-06-05 21:00:00 +0100

    oddOrEven7thKyu completed - all tested passed

--- codewars JavaScript Challenges/oddOrEven7thKyu.js
+++ codewars JavaScript Challenges/oddOrEven7thKyu.js
@@ -1,3 +1,24 @@
 function oddOrEven(array) {
-    
- }
+
+    var inputArr = new Array();
+
+    inputArr = array;
+
+    var i;
+    var sum=0;
+    for(i=0; i<=inputArr.length-1; i++) {
+
+        sum += inputArr[i];
+
+    }
+
+    if(sum%2){ 
+        return 'odd'
+    }else{ 
+        return 'even'
+
+    }
+
+}
+
+console.log(oddOrEven([0,-1,-5]));
